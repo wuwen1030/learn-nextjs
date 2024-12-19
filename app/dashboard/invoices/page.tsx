1e25165e1543c08b61565e166985773a17c142ae
xiabin <xiabin.xb@bytedance.com>
2024-12-19 17:06:53 +0800

    feat: chapter 16

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 import Pagination from "@/app/ui/invoices/pagination";
+import { Metadata } from "next";
 
 export default async function Page(props: {
   searchParams?: Promise<{
